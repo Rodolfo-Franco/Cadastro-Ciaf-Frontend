767ec7c21d3e3043b62a0c68cb38d9979996680f
Rodolfo Franco <rodolfo_franco@outlook.com.br>
2023-08-29 13:05:55 -0300

    adicionando vuetify no projeto

---
+++ plugins/vuetify.js
@@ -0,0 +1,18 @@
+// plugins/vuetify.js
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as labsComponents from 'vuetify/labs/components'
+import * as directives from 'vuetify/directives'
+
+export default defineNuxtPlugin(nuxtApp => {
+  const vuetify = createVuetify({
+    ssr: true,
+    components: {
+      ...components,
+      ...labsComponents
+    },
+    directives,
+  })
+
+  nuxtApp.vueApp.use(vuetify)
+})
